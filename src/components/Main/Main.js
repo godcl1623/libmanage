7cbff916427376fadb7162f65bc2e1b371a03b7c
godcl1623 <delphi5281@gmail.com>
2021-11-05 02:09:00 +0900

    test

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -145,6 +145,7 @@ const Main = () => {
           { withCredentials: true }
         )
         .then(res => {
+          console.log(res.data)
           if (res.data.isLoginSuccessful) {
             if (!res.data.isGuest) {
               dispatch(loginStatusCreator(res.data.isLoginSuccessful));
