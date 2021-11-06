3b826baf09f90bdcd59aeaf507662e107ffce396
godcl1623 <delphi5281@gmail.com>
2021-11-06 20:19:06 +0900

    libmanage-client - develop - 메타데이터 검색 개선 테스트 진행중

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -10,8 +10,19 @@ const Progress = () => {
   const [count, setCount] = useState('');
   const [total, setTotal] = useState('');
   const [status, setStatus] = useState('1');
+  // #################### 임시 작성 시작 ####################
+  const [userInfo, setUserInfo] = useState('');
+  const [apiKey, setApiKey] = useState('');
+  const [maxApiCall, setMaxApiCall] = useState('');
+  const [currApiCall, setCurrApiCall] = useState('');
+  // #################### 임시 작성 끝 ####################
   const history = useHistory();
   const dispatch = useDispatch();
+  const abortCon = new AbortController();
+  const forceAbort = (aborter, history) => setTimeout(() => {
+    aborter.abort();
+    history.push('/main');
+  }, 30000);
   const statusText = status => {
     switch (status) {
       case '1':
@@ -28,6 +39,7 @@ const Progress = () => {
         return '오류가 발생했습니다';
     }
   };
+  /* #################### 원본 코드 시작 ####################
   useEffect(() => {
     const abortCon = new AbortController();
     const message = {
@@ -40,8 +52,8 @@ const Progress = () => {
     }, 30000);
     axios
       // .post('http://localhost:3002/check_login', { message: '' }, { withCredentials: true })
-      // .post('http://localhost:3001/check_login', { message }, { withCredentials: true })
-      .post(`https://${sendTo}/check_login`, { message }, { withCredentials: true })
+      .post('http://localhost:3001/check_login', { message }, { withCredentials: true })
+      // .post(`https://${sendTo}/check_login`, { message }, { withCredentials: true })
       .then(res => {
         const reqUserInfo = res.data;
         return new Promise((resolve, reject) => resolve(reqUserInfo));
@@ -50,8 +62,8 @@ const Progress = () => {
         forceAbort();
         axios
           // .post('http://localhost:3003/api/search', { reqUserInfo: res }, { withCredentials: true })
-          // .post('http://localhost:3001/api/search', { reqUserInfo: res }, { withCredentials: true })
-          .post(`https://${sendTo}/api/search`, { reqUserInfo: res }, { withCredentials: true })
+          .post('http://localhost:3001/api/search', { reqUserInfo: res }, { withCredentials: true })
+          // .post(`https://${sendTo}/api/search`, { reqUserInfo: res }, { withCredentials: true })
           .then(res => {
             if (res.data.result) {
               clearTimeout(forceAbort);
@@ -65,7 +77,7 @@ const Progress = () => {
     return () => abortCon.abort();
   }, []);
   // useEffect(() => {
-  //   axios.post('http://localhost:3003/api/search', {}, {withCredentials: true})
+  //   axios.post('http://localhost:3003/api/search', {}, {withCredentials: ƒtrue})
   //     .then(res => {
   //       if (res.data) {
   //         setTimeout(() => history.push('/main'), 3000);
@@ -76,8 +88,104 @@ const Progress = () => {
     const abortCon = new AbortController();
     const requestStatus = setInterval(() => {
       // axios.post('http://localhost:3003/stat/track', {}, { withCredentials: true }).then(res => {
-      // axios.post('http://localhost:3001/stat/track', {}, { withCredentials: true }).then(res => {
-      axios.post(`https://${sendTo}/stat/track`, {}, { withCredentials: true }).then(res => {
+      axios.post('http://localhost:3001/stat/track', {}, { withCredentials: true }).then(res => {
+      // axios.post(`https://${sendTo}/stat/track`, {}, { withCredentials: true }).then(res => {
+        if (res.data.status === status) {
+          setCount(res.data.count);
+          setTotal(res.data.total);
+        } else {
+          setCount(res.data.count);
+          setTotal(res.data.total);
+          setStatus(res.data.status);
+        }
+        if (status === '5') {
+          clearInterval(requestStatus);
+        }
+      });
+    }, 1000);
+    return () => {
+      clearInterval(requestStatus);
+      abortCon.abort();
+    };
+  }, [total]);
+  #################### 원본 코드 끝 ####################
+  */
+  useEffect(() => {
+    const message = {
+      comparisonState,
+      million: localStorage.getItem('frog')
+    };
+    const timer = forceAbort(abortCon, history);
+    axios
+      // .post('http://localhost:3002/check_login', { message: '' }, { withCredentials: true })
+      .post('http://localhost:3001/check_login', { message }, { withCredentials: true })
+      // .post(`https://${sendTo}/check_login`, { message }, { withCredentials: true })
+      .then(res => {
+        const reqUserInfo = res.data;
+        clearTimeout(timer);
+        setUserInfo(reqUserInfo);
+      })
+      .catch(err => console.log('err occured', err));
+    return () => abortCon.abort();
+  }, []);
+  useEffect(() => {
+    if (userInfo !== '') {
+      const timer = forceAbort(abortCon, history);
+      axios
+        .get('http://localhost:3001/storeLib', { withCredentials: true })
+        .then(res => {
+          clearTimeout(timer);
+          setApiKey(res.data.apiKey);
+          setMaxApiCall(Math.ceil(res.data.maxGames/25));
+          setCurrApiCall(7);
+        })
+        .catch(err => console.log(err));
+    }
+    return () => abortCon.abort();
+  }, [userInfo]);
+  useEffect(() => {
+    if (currApiCall !== '') {
+      const pack = {
+        apiCred: apiKey,
+        maxApiCall,
+        currApiCall
+      }
+      const timer = forceAbort(abortCon, history);
+      axios
+        .post('http://localhost:3001/meta/search', { pack }, { withCredentials: true })
+        .then(res => {
+          clearTimeout(timer);
+          if (res.data === 'done') {
+            setCurrApiCall('done');
+          } else {
+            setCurrApiCall(currApiCall => currApiCall + Number(res.data));
+          }
+        })
+        .catch(err => console.log(err));
+    }
+      return () => abortCon.abort();
+  }, [currApiCall]);
+  useEffect(() => {
+    if (currApiCall === 'done') {
+      const timer = forceAbort(abortCon, history);
+      axios
+        .post('http://localhost:3001/api/search', { reqUserInfo: userInfo }, { withCredentials: true })
+        .then(res => {
+          if (res.data.result) {
+            clearTimeout(timer);
+            // dispatch(comparisonStateCreator(res.data.newInfo));
+            setTimeout(() => history.push('/main'), 1500);
+          }
+        })
+        .catch(err => console.log(err));
+    }
+    return () => abortCon.abort();
+  }, [currApiCall]);
+  useEffect(() => {
+    const requestStatus = setInterval(() => {
+      // axios.post('http://localhost:3003/stat/track', {}, { withCredentials: true }).then(res => {
+      axios.post('http://localhost:3001/stat/track', {}, { withCredentials: true }).then(res => {
+      // axios.post(`https://${sendTo}/stat/track`, {}, { withCredentials: true }).then(res => {
         if (res.data.status === status) {
           setCount(res.data.count);
           setTotal(res.data.total);
