2c907d4ba3ae88dc64087394ddde0df5b42672d1
godcl1623 <delphi5281@gmail.com>
2021-11-09 03:10:27 +0900

    libmanage-client - develop - 회원정보 수정을 위한 인증기능 추가, 회원정보 수정 및 탈퇴기능 구현 필요

--- src/components/Member/MemberInfoWrap.js
+++ src/components/Member/MemberInfoWrap.js
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import TabBtn from './parts/TabBtn';
 import ModMemInfo from './comps/ModMemInfo';
+import CheckMemInfo from './comps/CheckMemInfo';
 
 const MemberInfoWrap = () => {
   const modalState = useSelector(state => state.modalState);
+  const userState = useSelector(state => state.userState);
   const [tabState, setTabState] = useState('modify');
+  const [verifyState, setVerifyState] = useState(false);
   useEffect(() => {
     if (!modalState) setTabState('modify');
   }, [modalState]);
@@ -16,21 +19,29 @@ const MemberInfoWrap = () => {
       <div
         className='member-info-contents-wrap'
       >
-        <div
-          className='member-info-contents-tab-wrap'
-        >
-          <TabBtn
-            setState={ setTabState }
-            stateVal={ ['modify', 'withdraw'] }
-          >
-            { ['회원 정보 변경', '회원 탈퇴'] }
-          </TabBtn>
-        </div>
-        <div
-          className='member-info-contents'
-        >
-          {tabState === 'modify' ? <ModMemInfo /> : <h1>차가운무</h1>}
-        </div>
+        {
+          verifyState
+            ?
+              <>
+                <div
+                  className='member-info-contents-tab-wrap'
+                >
+                  <TabBtn
+                    setState={ setTabState }
+                    stateVal={ ['modify', 'withdraw'] }
+                  >
+                    { ['회원 정보 변경', '회원 탈퇴'] }
+                  </TabBtn>
+                </div>
+                <div
+                  className='member-info-contents'
+                >
+                  {tabState === 'modify' ? <ModMemInfo /> : <h1>차가운무</h1>}
+                </div>
+              </>
+            :
+              <CheckMemInfo userState={userState} setState={setVerifyState} />
+        }
       </div>
     </article>
   );
---
+++ src/components/Member/comps/CheckMemInfo.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import axios from 'axios';
+import { hasher, salter } from '../../../custom_modules/hasher';
+import { encryptor } from '../../../custom_modules/aeser';
+import FormSubmit from '../../Auth/module/components/FormSubmit';
+import { sendTo } from '../../../custom_modules/address';
+
+const CheckMemInfo = ({ userState, setState}) => (
+  <form
+    onSubmit={e => {
+      e.preventDefault();
+      const verificationData = {
+        NICK: '',
+        PWD: ''
+      };
+      if (e.target.PWD.value !== '') {
+        verificationData.NICK = userState.nickname;
+        verificationData.PWD = salter(hasher(e.target.PWD.value));
+      }
+      axios
+        .post(
+          // 'http://localhost:3001/verify',
+          `https://${sendTo}/verify`,
+          { sofo: encryptor(verificationData, process.env.REACT_APP_TRACER) },
+          { withCredentials: true }
+        )
+        .then(res => {
+          if (res.data) {
+            setState(res.data);
+          } else {
+            alert('비밀번호를 확인해주세요.');
+          }
+        })
+        .catch(err => alert(err));
+    }}
+  >
+    <p>본인 인증을 위해 비밀번호를 입력해주세요.</p>
+    <div
+      className="input_container"
+    >
+      <label htmlFor="PWD">PW: </label>
+      <input type="password" name="PWD" />
+    </div>
+    <FormSubmit />
+  </form>
+);
+
+export default CheckMemInfo;
