b02157b3a786c75fa657f13012a78c72fac6c853
godcl1623 <delphi5281@gmail.com>
2021-11-14 03:17:26 +0900

    libmanage-client - develop - 아이디/비밀번호 찾기 스타일 작성

--- src/components/Auth/module/components/FindRequested.js
+++ src/components/Auth/module/components/FindRequested.js
@@ -5,16 +5,26 @@ const FindRequested = ({ mode }) => {
   if (mode === 'pwd') {
     return (
       <>
-        <InputTemplate inputType="text" labelText="가입한 아이디: " inputFor="ID" />
-        <InputTemplate inputType="text" labelText="가입한 별명: " inputFor="nickname" />
-        <InputTemplate inputType="text" labelText="가입한 이메일 주소: " inputFor="email" />
+        <div className="verify_id">
+          <InputTemplate inputType="text" labelText="가입한 아이디" inputFor="ID" />
+        </div>
+        <div className="verify_nickname">
+          <InputTemplate inputType="text" labelText="가입한 별명" inputFor="nickname" />
+        </div>
+        <div className="verify_email">
+          <InputTemplate inputType="text" labelText="가입한 이메일 주소" inputFor="email" />
+        </div>
       </>
     );
   } 
   return (
     <>
-      <InputTemplate inputType="text" labelText="가입한 별명: " inputFor="nickname" />
-      <InputTemplate inputType="text" labelText="가입한 이메일 주소: " inputFor="email" />
+      <div className="verify_nickname">
+        <InputTemplate inputType="text" labelText="가입한 별명" inputFor="nickname" />
+      </div>
+      <div className="verify_email">
+        <InputTemplate inputType="text" labelText="가입한 이메일 주소" inputFor="email" />
+      </div>
     </>
   );
 }
