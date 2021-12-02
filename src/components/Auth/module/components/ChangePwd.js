973ec0fb84c2708917db56e718402d7bfe15f581
godcl1623 <delphi5281@gmail.com>
2021-12-03 03:05:10 +0900

    libmanage-client - develop - Auth 폴더 내 컴포넌트들 스타일 분리 완료

--- src/components/Auth/module/components/ChangePwd.js
+++ src/components/Auth/module/components/ChangePwd.js
@@ -13,6 +13,7 @@ import { hasher } from '../../../../custom_modules/hasher';
 import { verifyPwd } from '../utils';
 import { sendTo } from '../../../../custom_modules/address';
 import { border, flex, sizes } from '../../../../styles';
+import style from '../styles/components/ChangePwdStyles';
 
 const ChangePwd = ({ token, reqTime }) => {
   const [pwdMatch, setPwdMatch] = useState(true);
@@ -44,112 +45,7 @@ const ChangePwd = ({ token, reqTime }) => {
   return (
     <form
       css={css`
-        border-radius: var(--border-rad-big);
-        padding: calc(var(--gap-standard) * 2);
-        background: white;
-        ${sizes.free('40%', 'max-content')}
-        ${flex.vertical}
-        justify-content: space-between;
-
-        .input-wrapper {
-          ${flex.vertical}
-          align-items: flex-start;
-          ${sizes.free('100%')}
-          font-size: var(--font-size-normal);
-
-          * {
-            ${sizes.free('100%')}
-          }
-
-          input {
-            margin: calc(var(--gap-multiply-small) * 2) 0;
-            ${border}
-            border-color: var(--grey-dark);
-          }
-
-          .verify-error {
-            margin-bottom: calc(var(--gap-multiply-big) * 2);
-            color: red;
-            font-weight: bold;
-          }
-
-          .verify-error#input-pwd {
-            opacity: ${isValid ? '0' : '100%'};
-          }
-
-          .verify-error#input-pwd-check {
-            opacity: ${pwdMatch ? '0' : '100%'};
-          }
-        }
-
-        .submit-wrapper {
-          ${flex.horizontal}
-          ${sizes.free('100%', '2.604vw')}
-
-          button:first-of-type {
-            margin-right: var(--gap-multiply-small);
-            background: var(--btn-active);
-          }
-
-          button:last-of-type {
-            margin-left: var(--gap-multiply-small);
-            background: var(--btn-active);
-          }
-        }
-
-        @media (orientation: portrait) {
-          @media (min-width: 600px) {
-            ${sizes.free('60%', 'max-content')}
-  
-            .submit-wrapper {
-              ${sizes.free('100%', `${2.604 * 1.778}vw`)}
-  
-              button:first-of-type {
-                margin-right: calc(var(--gap-multiply-small) * 2);
-              }
-  
-              button:last-of-type {
-                margin-left: calc(var(--gap-multiply-small) * 2);
-              }
-            }
-          }
-
-          @media (max-width: 599px) {
-            border-radius: 10px;
-            padding: 20px;
-            ${sizes.free('90%', 'max-content')}
-
-            .input-wrapper {
-              font-size: 16px;
-
-              input {
-                margin: 20px 0;;
-                padding: 5px 10px;
-                font-size: 14px;
-              }
-
-              .verify-error {
-                margin-bottom: 20px;
-                font-size: 14px;
-              }
-            }
-
-            .submit-wrapper {
-              ${sizes.free('100%', '30px')}
-              button {
-                font-size: 13px;
-              }
-
-              button:first-of-type {
-                margin-right: var(--gap-multiply-small);
-              }
-
-              button:last-of-type {
-                margin-left: var(--gap-multiply-small);
-              }
-            }
-          }
-        }
+        ${style({ sizes, flex, border }, { isValid, pwdMatch })}
       `}
       onSubmit={e => {
         e.preventDefault();
