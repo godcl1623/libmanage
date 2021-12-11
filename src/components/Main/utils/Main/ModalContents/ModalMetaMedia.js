a40becfbfbe2877c9540b97309a249929d9913ae
godcl1623 <delphi5281@gmail.com>
2021-12-11 20:52:58 +0900

    libmanage-client - develop - main 스타일 분리

--- src/components/Main/utils/Main/ModalContents/ModalMetaMedia.js
+++ src/components/Main/utils/Main/ModalContents/ModalMetaMedia.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { css } from '@emotion/react';
 import { AiOutlineCloseCircle, AiOutlineRight, AiOutlineLeft } from 'react-icons/ai';
 import { sizes, flex } from '../../../../../styles';
+import { metaModalStyles } from '../../../styles/modals/ModalContentsStyles';
 
 const ModalMetaMedia = ({ props }) => {
   const {
@@ -15,69 +16,9 @@ const ModalMetaMedia = ({ props }) => {
   } = props;
 
   return (
-    <div
-      css={css`
-        padding: calc(var(--gap-standard) * 2);
-        ${sizes.full}
-        ${flex.vertical}
-        position: relative;
-
-        @media (orientation: portrait) and (max-width: 599px) {
-          padding: 10px;
-        }
-      `}
-    >
+    <div css={css`${metaModalStyles({ sizes, flex })}`}>
       <span
         className="modal-close"
-        css={css`
-          border-radius: 50%;
-          position: absolute;
-          top: -1.667vw;
-          right: -1.667vw;
-          cursor: pointer;
-          background: white;
-          ${flex.vertical}
-          ${sizes.free('1.667vw', '1.667vw')}
-
-          svg {
-            font-size: 1.667vw;
-          }
-
-          @media (max-width: 720px) {
-            top: 0;
-            right: -20px;
-            ${sizes.free('16px', '16px')}
-
-            svg {
-              font-size: 32px;
-            }
-          }
-
-          @media (orientation: portrait) {
-            @media (min-width: 600px) {
-              top: -${1.667 * 1.778}vw;
-              right: -${1.667 * 1.778}vw;
-              ${sizes.free(`${1.667 * 1.778}vw`, `${1.667 * 1.778}vw`)}
-  
-              svg {
-                font-size: ${1.667 * 1.778}vw;
-              }
-            }
-
-            @media (max-width: 599px) {
-              top: 10px;
-              right: 50%;
-              transform: translateX(50%);
-              ${sizes.free('32px', '32px')}
-              opacity: 50%;
-              z-index: 9;
-
-              svg {
-                font-size: 32px;
-              }
-            }
-          }
-        `}
         onClick={e => {
           dispatch(modalStateCreator(false));
           dispatch(selectedMediaIdCreator(''));
@@ -85,12 +26,7 @@ const ModalMetaMedia = ({ props }) => {
       >
         <AiOutlineCloseCircle />
       </span>
-      <div
-        className="contents-wrapper"
-        css={css`
-          ${sizes.full}
-        `}
-      >
+      <div className="contents-wrapper">
         {
           selectedMediaId
             ? target === 'videos'
@@ -112,74 +48,11 @@ const ModalMetaMedia = ({ props }) => {
                       src={`https://images.igdb.com/igdb/image/upload/t_original/${selectedMediaId}.jpg`}
                       alt="media"
                       id={`img-${selectedMediaId}`}
-                      css={css`
-                        ${sizes.full}
-                        color: white;
-                      `}
                     />
                   )
             : ''
         }
-        <div
-          className="btn-wrapper"
-          css={css`
-            ${sizes.free('calc(100% - 4.167vw)', 'calc(100% - 4.167vw)')}
-            ${flex.horizontal}
-            justify-content: space-between;
-            position: absolute;
-            top: calc(var(--gap-standard) * 2);
-            left: calc(var(--gap-standard) * 2);
-
-            span {
-              ${flex.vertical}
-              ${sizes.free('15%', '100%')}
-              background: rgba(0, 0, 0, 0.5);
-              opacity: 0;
-              color: white;
-              font-size: 5.208vw;
-              text-weight: 900;
-              transition: all 0.3s;
-
-              * {
-                color: white;
-              }
-
-              :hover {
-                opacity: 100%;
-                cursor: pointer;
-              }
-
-              :active {
-                -webkit-filter: brightness(0.3);
-                filter: brightness(0.3);
-              }
-            }
-
-            @media (orientation: portrait) {
-              @media (min-width: 600px) {
-                ${sizes.free(
-                  `calc(100% - ${4.167 * 1.778}vw)`,
-                  `calc(100% - ${4.167 * 1.778}vw)`
-                )}
-  
-                span {
-                  font-size: ${5.208 * 1.778}vw;
-                }
-              }
-
-              @media (max-width: 599px) {
-                ${sizes.free('calc(100% - 20px)', 'calc(100% - 20px)')}
-                top: 10px;
-                left: 10px;
-
-                span {
-                  ${sizes.free('15%', '100%')}
-                  font-size: 50px;
-                }
-              }
-            }
-          `}
-        >
+        <div className="btn-wrapper">
           <span
             id="media-left"
             onClick={e => {
