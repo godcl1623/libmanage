e5e0e34ab4956a7dea0e194932c4734f319affe6
godcl1623 <delphi5281@gmail.com>
2021-12-20 01:18:28 +0900

    libmanage-client - develop - 스크롤 쓰로틀링 적용, 리스트 컴포넌트에 lazy 로드 적용

--- src/components/Main/utils/Header/SearchField.js
+++ src/components/Main/utils/Header/SearchField.js
@@ -4,31 +4,46 @@ import { css } from '@emotion/react';
 import { sizes, flex, border } from '../../../../styles';
 import { searchStyleForm, searchStyleBtn } from '../../styles/HeaderStyles';
 
-const SearchField = ({dispatch, setState, fieldVal}) => (
-  <form
-    css={css`${searchStyleForm({ border, flex, sizes })}`}
-  >
-    <input
-      type="text"
-      placeholder="검색어를 입력하세요"
-      name="libraryFilter"
-      onChange={e => {
+const SearchField = ({dispatch, setState, fieldVal}) => {
+  React.useEffect(() => {
+    let timer;
+    const debouncedDispatch = e => {
+      if (timer) {
+        clearTimeout(timer);
+      }
+      timer = setTimeout(() => {
         dispatch(setState(e.target.value));
-      }}
-    />
-    <button
-      name="delete-input"
-      id="delete-input"
-      css={css`${searchStyleBtn({ sizes }, { fieldVal })}`}
-      onClick={e => {
-        e.preventDefault();
-        e.target.parentNode.libraryFilter.value = '';
-        dispatch(setState(''));
-      }}
+      }, 500);
+    };
+    const searchField = document.querySelector('input#search_field');
+    searchField.addEventListener('input', debouncedDispatch);
+    return () => searchField.removeEventListener('input', debouncedDispatch);
+  }, []);
+
+  return (
+    <form
+      css={css`${searchStyleForm({ border, flex, sizes })}`}
     >
-      ×
-    </button>
-  </form>
-);
+      <input
+        type="text"
+        id="search_field"
+        placeholder="검색어를 입력하세요"
+        name="libraryFilter"
+      />
+      <button
+        name="delete-input"
+        id="delete-input"
+        css={css`${searchStyleBtn({ sizes }, { fieldVal })}`}
+        onClick={e => {
+          e.preventDefault();
+          e.target.parentNode.libraryFilter.value = '';
+          dispatch(setState(''));
+        }}
+      >
+        ×
+      </button>
+    </form>
+  )
+};
 
 export default SearchField;
