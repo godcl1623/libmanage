d3d01398ff852fae65f4644c510047844eeda2d4
godcl1623 <delphi5281@gmail.com>
2021-12-20 19:50:13 +0900

    libmanage-client - release - address.js 수정

--- src/custom_modules/address.js
+++ src/custom_modules/address.js
@@ -1,2 +1,3 @@
 /* eslint-disable import/prefer-default-export */
-export const sendTo = process.env.REACT_APP_SENDTO || 'libmanage-server.herokuapp.com';
+// export const sendTo = process.env.REACT_APP_SENDTO || 'libmanage-server.herokuapp.com';
+export const sendTo = 'libmanage-server.herokuapp.com';
