eefd4cdb644bdec934c356addffcd9389b9d8510
godcl1623 <delphi5281@gmail.com>
2022-07-02 21:31:05 +0900

    libmanage-client - features - 페이지 최초 오픈시 자동 캐시 기능 구현, 업데이트 후 캐시 기능 구현 필요

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,76 +1,80 @@
 // const currentNum = 2;
-let CACHE_NAME = 'my-app-cache-v1'
+const OLD_CACHE = 'my-app-cache-v2';
+const CACHE_NAME = 'my-app-cache-v1';
+const _window = this || self || window;
 // + String(currentNum - 1);
 
-const urlsToCache = [
-  '/'
-];
+const urlsToCache = ['/'];
 
-// // 추가
-// if ('serviceWorker' in navigator) {
-//   navigator.serviceWorker.getRegistrations().then(registrations => {
-//     for (const registration of registrations) {
-//       registration.update();
-//     }
-//   })
-// }
+// 추가
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(registrations => {
+    for (const registration of registrations) {
+      registration.update();
+    }
+  });
+}
 
 self.addEventListener('install', event => {
   // Perform install steps
   event.waitUntil(
-    caches.open(CACHE_NAME)
-    .then(cache => {
+    caches.open(CACHE_NAME).then(cache => {
       console.log('Opened cache');
       return cache.addAll(urlsToCache);
-    })
+    }).then(e => self.skipWaiting())
   );
-  self.skipWaiting();
+  // self.skipWaiting();
 });
 
 self.addEventListener('fetch', event => {
   if (event.request.method === 'POST' || event.request.method === 'PUT') return;
   if (!(event.request.url.indexOf('http') === 0)) return;
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        console.log('[Service Worker] Fetching resources: ', event.request.url);
-        return response ||
+    caches.match(event.request).then(response => {
+      console.log('[Service Worker] Fetching resources: ', event.request.url);
+      return (
+        response ||
         fetch(event.request)
           .then(response =>
-            caches.open(CACHE_NAME)
-              .then(cache => {
-                console.log('[Service Worker] Caching new resources: ', event.request.url);
-                cache.put(event.request, response.clone());
-                return response;
-              })
+            caches.open(CACHE_NAME).then(cache => {
+              console.log('[Service Worker] Caching new resources: ', event.request.url);
+              cache.put(event.request, response.clone());
+              return response;
+            })
           )
           .catch(err => {
             const cache = caches.open(CACHE_NAME);
           })
-      })
+      );
+    })
   );
 });
 
-// // 추가
-// self.addEventListener('activate', e => {
-//   e.waitUntil(
-//     caches.keys()
-//       .then(cacheNames => Promise.all(
-//         cacheNames.filter(cacheName => {
-//           if (!cacheName.endsWith('v')) {
-//             return true;
-//           }
-//         }).map(cacheName => {
-//           if ('serviceWorker' in navigator) {
-//             navigator.serviceWorker.getRegistrations().then(registrations => {
-//               registrations.map(r => r.unregister());
-//             });
-//             window.location.reload(true);
-//           }
-
-//           console.log('Removing old cache.', cacheName);
-//           return caches.delete(cacheName);
-//         })
-//       ))
-//   )
-// })
+// 추가
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames
+          .filter(cacheName => cacheName === OLD_CACHE)
+          .map(cacheName => {
+            
+            console.log('Removing old cache.', cacheName);
+            return caches.delete(cacheName);
+          })
+      )
+      .then(delCacheRes => {
+        if (delCacheRes[0] != null) {
+          // if ('serviceWorker' in navigator) {
+          //   navigator.serviceWorker.getRegistrations().then(registrations => {
+          //     registrations.map(r => r.unregister());
+          //   });
+          //   window.location.reload(true);
+          // }
+          
+          this.location.reload();
+        }
+      })
+    )
+  );
+});
